05add98f6033af30e09b021bfff5417b51192ffb
Maxwell Kendall <maxwell.n.kendall@gmail.com>
2019-08-06 11:03:48 -0400

    using new documentation components

--- src/pages/TooltipPage.jsx
+++ src/pages/TooltipPage.jsx
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 
+import CodeSnippet from "../docs/codeSnippet";
 import Tooltip from "../components/Tooltip";
+import Demo from "../docs/Demo";
+import DocumentationWrapper from '../docs/documentationWrapper';
 
 const ExampleTooltipContent = () => (
     <div className="tt__container">
@@ -9,13 +12,18 @@ const ExampleTooltipContent = () => (
         <p>Its really pretty neat and easy to use. I think youre gonna like it a ton.</p>
         <p>Youre welcome.</p>
     </div>
-)
+);
+
+const detail = "The tooltip component is a really neat component and this is the explaination of all of it.";
 
 class TooltipPage extends Component {
     render() {
         return (
-            <div className="component-page tooltip__page">
-                <div className="demo">
+            <DocumentationWrapper
+                className="tooltip__page"
+                header="ToolTipWrapper.jsx"
+                detail={detail}>
+                <Demo>
                     <Tooltip tooltipComponent={<ExampleTooltipContent />} offsetAdjustments={{ top: 0, right: 30 }}>
                         <p>Hover on me for a right-justified tooltip</p>
                     </Tooltip>
@@ -31,8 +39,13 @@ class TooltipPage extends Component {
                     <Tooltip left wide tooltipComponent={<ExampleTooltipContent />} offsetAdjustments={{ top: 0, right: 15 }}>
                         <p>Hover on me for a wide left-justified tooltip</p>
                     </Tooltip>
-                </div>
-            </div>
+                </Demo>
+                <CodeSnippet>
+{`<Tooltip left wide tooltipComponent={<ExampleTooltipContent />} offsetAdjustments={{ top: 0, right: 15 }}>
+    <p>Hover on me for a wide left-justified tooltip</p>
+</Tooltip>`}
+                </CodeSnippet>
+            </DocumentationWrapper>
         );
     }
 }
