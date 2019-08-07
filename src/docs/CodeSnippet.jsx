71b6449b5753721617e8ccd8425e960928e3d372
Maxwell Kendall <maxwell.n.kendall@gmail.com>
2019-08-07 17:09:07 -0400

    adding prop descriptions

--- src/docs/CodeSnippet.jsx
+++ src/docs/CodeSnippet.jsx
@@ -3,13 +3,30 @@
 import React from 'react';
 import CodeBlock from '@tenon-io/tenon-codeblock';
 
+export const buildPropDetails = (prop, propType, desc) => {
+    return {
+        [`${prop} (${propType})`]: desc
+    };
+};
+
 const CodeSnippet = ({
     propMap,
-    children
+    children,
+    propDetailsMap = { 'test (string)': "This is a definition of the prop" }
 }) => {
-    const x = '';
+    const propDetails = Object.keys(propDetailsMap)
+        .map((prop) => (
+            <li className="prop-detail">
+                <span className="prop-name">{prop} </span>
+                <span className="prop-description">{propDetailsMap[prop]}</span>
+            </li>
+        ));
+
     return (
         <div className="code-snippet">
+            <ul className="prop-details">
+                {propDetails}
+            </ul>
             <CodeBlock codeString={children} />
         </div>
     );
--- src/pages/TooltipPage.jsx
+++ src/pages/TooltipPage.jsx
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
 
-import CodeSnippet from "../docs/codeSnippet";
+import CodeSnippet from "../docs/CodeSnippet";
 import Tooltip from "../components/Tooltip";
 import Demo from "../docs/Demo";
-import DocumentationWrapper from '../docs/documentationWrapper';
+import DocumentationWrapper from "../docs/DocumentationWrapper";
+import { buildPropDetails } from "../docs/CodeSnippet";
 
 const ExampleTooltipContent = () => (
     <div className="tt__container">
@@ -22,12 +23,21 @@ const detail = (
 
 class TooltipPage extends Component {
     render() {
+        const propDetails = {
+            ...buildPropDetails('children', 'Node', 'This is the hover area which will activate the tooltip.'),
+            ...buildPropDetails('tooltipComponent', 'Node', 'The tooltip to be shown to the user.'),
+            ...buildPropDetails('left', 'Boolean', 'Justify tooltip to the left of the hover area. Default is justify to the right.'),
+            ...buildPropDetails('wide', 'Boolean', 'Expand the tooltip width to account for a larger tooltip.'),
+            ...buildPropDetails('controlledProps', 'Object', 'A configuration object to override local state (showTooltip) and local class methods (showTooltip & closeTooltip'),
+            ...buildPropDetails('offsetAdjustments', 'Object', 'A configuration object to override top/right/left positioning.'),
+            ...buildPropDetails('styles', 'Object', 'Configuration object to set inline css for the tooltip-wrapper <div> element.')
+        };
         return (
             <DocumentationWrapper
                 className="tooltip__page"
                 header="ToolTipWrapper.jsx"
                 detail={detail}>
-                <Demo>
+                <Demo prop>
                     <Tooltip tooltipComponent={<ExampleTooltipContent />} offsetAdjustments={{ top: 0, right: 30 }}>
                         <p>Hover on me for a right-justified tooltip</p>
                     </Tooltip>
@@ -44,7 +54,7 @@ class TooltipPage extends Component {
                         <p>Hover on me for a wide left-justified tooltip</p>
                     </Tooltip>
                 </Demo>
-                <CodeSnippet>
+                <CodeSnippet propDetailsMap={propDetails}>
 {`<Tooltip left wide tooltipComponent={<ExampleTooltipContent />} offsetAdjustments={{ top: 0, right: 15 }}>
     <p>Hover on me for a wide left-justified tooltip</p>
 </Tooltip>`}
